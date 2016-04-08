54d1be668e88e0eec5a795fa612c76d1ae1c1d42
Matthias Bach <matthias.bach@blue-yonder.com>
2016-04-08 10:03:57 +0200

    Change development status declaration to beta.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     ],
     test_suite='nose.collector',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',

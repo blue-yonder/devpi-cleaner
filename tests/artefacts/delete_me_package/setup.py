388943534da73c99e2da349461a12f6c81ad3a11
Matthias Bach <marix@marix.org>
2016-04-29 15:42:58 +0200

    Remove Blue Yonder identities from example artefact

--- tests/artefacts/delete_me_package/setup.py
+++ tests/artefacts/delete_me_package/setup.py
@@ -6,8 +6,8 @@ setup(
     name='delete_me',
     version='0.2a1',
     description="""Dummy artefact for devpi_cleaner tests.""",
-    author='Blue Yonder Technology Foundation Team',
-    url='http://jira.phi-tps.local',
-    author_email='Blue-Yonder.TeamTechnologie@blue-yonder.com',
+    author='Max Mustermann',
+    url='http://example.com',
+    author_email='mustermann@example.com',
     packages=['delete_me'],
 )

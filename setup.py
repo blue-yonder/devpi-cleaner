c7fb2da94fa461d8174e77c6ab727581f910d6b0
Matthias Bach <matthias.bach@blueyonder.com>
2023-10-30 16:34:14 +0100

    Run tests on Python 3.12

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: System :: Archiving :: Packaging',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
